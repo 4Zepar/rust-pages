1fea30d2db38299a3f1f08cd53c664646edb9732
zepar <krazerlord@gmail.com>
2025-01-30 18:38:30 +0300

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+let menuButton = document.querySelector(".menu");
+let menu = document.querySelector(".sidenav");
+let closeMenuButton = document.querySelector(".button-close");
+
+let zeroScale = () => menuButton.style.transform = 'scale(1)';
+
+const toggleMenu = (isOpen) => {
+    menuButton.style.transform = isOpen ? 'rotate(180deg)' : 'rotate(0)';
+    menuButton.style.transform = isOpen ? 'scale(1.05)' : 'scale(1)';
+};
+
+menuButton.addEventListener('mouseover', () => toggleMenu(true));
+menuButton.addEventListener('mouseout', () => toggleMenu(false));
+
+menuButton.addEventListener('click', () => {
+    toggleMenu(true);
+    setTimeout(zeroScale, 200);
+});
+
+const toggleContentMenu = (isOpen) => {
+    menu.style.transform = isOpen ? 'translateX(0%)' : 'translateX(-105%)';
+    closeMenuButton.style.transform = isOpen ? 'rotate(90deg)' : 'rotate(-90deg)';
+    closeMenuButton.style.scale = isOpen ? '1' : '0';
+};
+
+menuButton.addEventListener('click', () => toggleContentMenu(true));
+closeMenuButton.addEventListener('click', () => toggleContentMenu(false));
